173bf9ad93755d01ff56c32d18f8f33529e89f95
Tyler Mandry <tmandry@gmail.com>
2018-11-21 17:45:52 -0600

    fish: Fix keybindings for 2.7

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 set -x EDITOR vim
-#set -U fish_key_bindings fish_user_key_bindings
 
 abbr apt aptitude
 
@@ -15,3 +14,22 @@ abbr ga git add
 abbr gl1 git log -1
 abbr fx git commit -m \'fx\'
 abbr afx git commit -am \'fx\'
+
+if status --is-interactive
+  function fish_user_key_bindings
+    # fish_vi_key_bindings: set below
+
+    # use default bindings for everything in insert mode
+    fish_default_key_bindings -M insert
+    # ..except escape, which exits insert mode
+    bind -M insert \e "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+
+    bind -M insert \ce forward-word
+  end
+
+  set -g fish_key_bindings fish_vi_key_bindings
+end
+
+function fish_mode_prompt
+  # NOOP - disable default mode indicator before prompt
+end
