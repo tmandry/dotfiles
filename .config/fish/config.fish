cfae03ff1138cce67daa8e0e098d370a27f68c26
Tyler Mandry <tmandry@gmail.com>
2019-08-12 11:28:31 -0700

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,5 @@
 set -x EDITOR vim
 
-abbr apt aptitude
-
 abbr be bundle exec
 
 abbr g git
@@ -13,23 +11,24 @@ abbr gs git status
 abbr gd git diff
 abbr ga git add
 abbr gl1 git log -1
-abbr fx git commit -m \'fx\'
+abbr gl git log
+#abbr fx git commit -m \'fx\'
 abbr afx git commit -am \'fx\'
 
-if status --is-interactive
-  function fish_user_key_bindings
-    # fish_vi_key_bindings: set below
-
-    # use default bindings for everything in insert mode
-    fish_default_key_bindings -M insert
-    # ..except escape, which exits insert mode
-    bind -M insert \e "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
-
-    bind -M insert \ce forward-word
-  end
-
-  set -g fish_key_bindings fish_vi_key_bindings
-end
+#if status --is-interactive
+#  function fish_user_key_bindings
+#    # fish_vi_key_bindings: set below
+#
+#    # use default bindings for everything in insert mode
+#    fish_default_key_bindings -M insert
+#    # ..except escape, which exits insert mode
+#    bind -M insert \e "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+#
+#    bind -M insert \ce forward-word
+#  end
+#
+#  set -g fish_key_bindings fish_vi_key_bindings
+#end
 
 function fish_mode_prompt
   # NOOP - disable default mode indicator before prompt
