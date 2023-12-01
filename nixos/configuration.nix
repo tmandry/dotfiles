0fafe0616c3f3e5e312e8de6b13c476293439a21
Tyler Mandry <tmandry@gmail.com>
2023-11-30 20:24:10 -0800

    caps lock escape

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -58,8 +58,8 @@
   programs.zsh.enable = true;
 
   # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.options = "caps:escape";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
