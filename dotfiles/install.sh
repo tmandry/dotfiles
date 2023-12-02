fc2613feb9b90166cf30c51d00126a1eb16db70d
Tyler Mandry <tmandry@gmail.com>
2023-12-01 20:08:13 -0800

    Move dotfiles into dotfiles/

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Requires GNU utils.
     CP=gcp
     FIND=gfind
+    REALPATH=grealpath
     if [[ ! -x $(command -v $CP) || ! -x $(command -v $FIND) ]]; then
       echo "Requires $CP and $FIND."
       echo "Please install with: brew install coreutils findutils"
@@ -13,15 +14,19 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 else
     CP=cp
     FIND=find
+    REALPATH=realpath
 fi
 
+dotfiles=$(dirname $($REALPATH --relative-to=$HOME $(pwd)/$0))
+
 cd $HOME
+
 echo "Backing up existing files to dotfiles-backup.."
 [ -d dotfiles-backup ] || mkdir dotfiles-backup
 BACKUP_FILES=$(cd ~/.dotfiles; "$FIND" . -name '.git' -prune -o -exec test -e ~/'{}' ';' -and -exec test -f ~/'{}' ';' -and -exec test ! -L ~/'{}' ';' -print)
 for f in $BACKUP_FILES; do
   echo $f
-  mkdir -p ~/dotfiles-backup/$(dirname "$f") && mv -fnv ~/$f dotfiles-backup/$f
+   mkdir -p ~/dotfiles-backup/$(dirname "$f") && mv -fnv ~/$f dotfiles-backup/$f
 done
 #echo $BACKUP_FILES | xargs -n 1 echo
 #echo $BACKUP_FILES | xargs -I {} -n 1 echo sh -c 'mkdir -p ~/.dotfiles-backup/$(dirname "{}") && mv -fn {} .dotfiles-backup/{}'
@@ -29,9 +34,8 @@ echo "done."
 
 echo
 echo "Linking files in .dotfiles.."
-cd "$HOME/.dotfiles"
-# Glob matches all files with a dot in front (excluding . and ..)
-"$FIND" . -mindepth 1 -maxdepth 1 -not '(' -name '.git' -o -name 'install.sh' ')' -print -exec "$CP" -Rfs "$HOME/.dotfiles/{}" "$HOME/" ';'
+# Glob matches all files with a dot in front (excluding . d ..)
+"$FIND" $HOME/$dotfiles -mindepth 1 -maxdepth 1 -not '(' -name '.git' -o -name 'install.sh' ')' -exec "$CP" -Rfvs "{}" . ';'
 
 echo "done."
 
