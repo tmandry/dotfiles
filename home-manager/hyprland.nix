89b19c976306a95d2360bb8da58684d6f48634e5
Tyler Mandry <tmandry@gmail.com>
2023-12-02 19:30:03 -0800

    Try darkman - not working

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -8,6 +8,7 @@
   home.packages = with pkgs; [
     lxqt.lxqt-policykit
 
+    darkman
     dunst
     pipewire
     light
@@ -18,7 +19,7 @@
 
     # firefox
 
-    pkgs.font-awesome
+    font-awesome
   ];
 
   programs.waybar = {
@@ -29,6 +30,17 @@
       modules-right = [ "idle_inhibitor" "pulseaudio" "network" "cpu" "memory" "temperature" "backlight" "keyboard-state" "battery" "clock" "tray" ];
     }];
   };
+
+  services.darkman = {
+    enable = true;
+    settings.usegeoclue = false;
+    settings.lat = 37.758377;
+    settings.lng = -122.388518;
+  };
+  home.file.".config/xdg-desktop-portal/portals.conf".text = ''
+    [preferred]
+    org.freedesktop.impl.portal.Settings=darkman
+  '';
 
   fonts.fontconfig.enable = true;
 }
